7d06ac17cce7b1aac968c42b6adb9f8953e2092b
Adi Duggal <duggaadi25@7hills.org>
2023-08-30 13:00:18 -0400

    Created a file with "Hello World!" and "Scambot"

---
+++ Receipt.java
@@ -0,0 +1,16 @@
+// Receipt.java
+import java.util.Scanner;
+
+public class Receipt {
+    
+    public static void main(String[] args) {
+
+        Scanner ScamBot = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+
+        String hello = ScamBot.nextLine();
+        System.out.println("Ok");
+
+    }
+}
