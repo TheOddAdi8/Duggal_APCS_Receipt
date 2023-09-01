251c414540821c68def3041c4cf04a071bb55018
Adi Duggal <duggaadi25@7hills.org>
2023-09-01 12:27:15 -0400

    eggs and milk

--- Receipt.java
+++ Receipt.java
@@ -5,10 +5,21 @@ public class Receipt {
     
     public static void main(String[] args) {
 
-        Scanner ScamBot = new Scanner(System.in);
+        Scanner orderBot = new Scanner(System.in);
+
+        int milk;
+        double eggs;
 
         System.out.println("Welcome to Adi's Convience Store!");
-        System.out.println("")
+        System.out.println("We offer a wide selection of products.");
+
+        System.out.print("Enter how many gallons of milk you want: ");
+        milk = orderBot.nextInt();
+        System.out.print("Enter how many eggs you want (decimals allowed): ");
+        eggs = orderBot.nextDouble();
+
+
+        orderBot.close();
 
     }
 }
