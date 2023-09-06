8bc5e0079de6c3395324cc34b192357ba5d6993d
Adi Duggal <duggaadi25@7hills.org>
2023-09-06 12:18:38 -0400

    added clear() method

--- Receipt.java
+++ Receipt.java
@@ -6,6 +6,10 @@ public class Receipt {
     static void astPrint() { //print asterisk
         System.out.print("*");
       }
+    
+    static void clear() { //adds empty line
+        System.out.println("");
+    }
 
     public static void main(String[] args) {
 
@@ -16,29 +20,29 @@ public class Receipt {
         int O2 = -1;
         int H2 = -1;
 
-        System.out.println("");
+        clear();
         System.out.println("Welcome to Adi's Convience Store!");
         System.out.println("We offer a wide selection of products.");
 
-        System.out.println("");
+        clear();
         System.out.print("Enter how many gallons of milk you want: ");
         while(milk < 0) {
             milk = orderBot.nextInt();
         }
 
-        System.out.println("");
+        clear();
         System.out.print("Enter how many eggs you want (decimals allowed): ");
         while(eggs < 0) {
             eggs = orderBot.nextDouble();
         }
 
-        System.out.println("");
+        clear();
         System.out.print("Enter how many moles (6.02 x 10^23 molecules)(1 mole of O2 is about 32 grams) of oxygen (O2) you want: ");
         while(O2 < 0) {
             O2 = orderBot.nextInt();
         }
 
-        System.out.println("");
+        clear();
         System.out.println("We don't sell water, so you can buy the Hydrogen individually.");
         System.out.print("Enter how many moles (6.02 x 10^23 molecules)(1 mole of H2 is about 2 grams) of hydrogen (H2) you want: ");
         while(H2 < 0) {
@@ -50,14 +54,15 @@ public class Receipt {
         final double MILKPRICE = 9.99;
         final double EGGS = 0.49;
 
-        System.out.println("");
-        System.out.println("");
+        clear();
+        clear();
 
         for (int i = 0; i < 40; i++) {
             astPrint();
         }
-        System.out.println("");
+        clear();
         astPrint();
+        System.out.println
         
 
 
